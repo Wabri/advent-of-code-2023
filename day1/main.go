7e3e4f6dd6cb05f2d3a78acaa8cde3baae8f79aa
Wabri <12409541+Wabri@users.noreply.github.com>
2023-12-02 13:03:02 +0100

    feat(day1_part2): try to solve with this
    The answer seems not to be correct unless the test file is give me the
    right answer
    
    I think there is something wrong with single number in line that is not
    explain how to handle or something linked the priority to reversed
    numbers, like eightwo is 82 or only 2 because the t of the string is
    already used? dont know, need more test anyway!

--- day1/main.go
+++ day1/main.go
@@ -4,43 +4,115 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"unicode"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.Open("input_2.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
 	}
 	defer file.Close()
-	scanner := bufio.NewReader(file)
+	scanner := bufio.NewScanner(file)
 	totalCalibration := 0
-	firstDigit := -1
-	lastDigit := -1
 
-	for {
-		char, _, err := scanner.ReadRune()
-		if err != nil {
-			break
-		}
+	for scanner.Scan() {
+		line := scanner.Text()
 
-		if unicode.IsDigit(char) {
-			if firstDigit == -1 {
-				firstDigit = int(char) - 48
-			} else {
-				lastDigit = int(char) - 48
-			}
-		} else if char == '\n' {
-			if lastDigit == -1 {
-				lastDigit = firstDigit
-			}
-			totalCalibration = totalCalibration + firstDigit*10 + lastDigit
-			firstDigit = -1
-			lastDigit = -1
-		}
+		firstDigit := extractFrom(line)
+		line = reverseButKeepOrder(line)
+		lastDigit := extractFrom(line)
+		// lastDigit := 0
+		// if len(line) > 1 {
+		// 	lastDigit = extractFrom(line)
+		// } else {
+		// 	lastDigit = firstDigit
+		// 	firstDigit = 0
+		// }
+		totalCalibration += firstDigit*10 + lastDigit
 	}
 
 	fmt.Println(totalCalibration)
+}
 
+func reverseButKeepOrder(word string) string {
+	var alphaPart string
+	result := ""
+	for _, char := range word {
+		if unicode.IsLetter(char) {
+			alphaPart += string(char)
+			digit := extractDigit(alphaPart)
+			if digit != -1 {
+				result = strconv.Itoa(digit) + result
+				alphaPart = alphaPart[1:]
+			}
+		} else if unicode.IsDigit(char) {
+			digit := extractDigit(alphaPart)
+			if digit != -1 {
+				result = strconv.Itoa(digit) + result
+			}
+			result = string(char) + result
+			alphaPart = ""
+		}
+	}
+
+	return result
+}
+
+func extractFrom(line string) int {
+	characters := ""
+	for _, char := range line {
+		if unicode.IsDigit(char) {
+			return int(char - '0')
+		} else {
+			characters += string(char)
+		}
+		if len(characters) >= 3 {
+			digit := 0
+			digit = extractDigit(characters)
+			if digit != -1 {
+				return digit
+			}
+		}
+	}
+	return -1
+}
+
+func extractDigit(word string) int {
+	for i := 3; i <= len(word) && i <= 5; i++ {
+		isDigit, digit := isADigit(word[0:i])
+		if isDigit {
+			return digit
+		}
+	}
+	if len(word) < 3 {
+		return -1
+	}
+	return extractDigit(word[1:])
+}
+
+func isADigit(word string) (bool, int) {
+	switch word {
+	case "one":
+		return true, 1
+	case "two":
+		return true, 2
+	case "three":
+		return true, 3
+	case "four":
+		return true, 4
+	case "five":
+		return true, 5
+	case "six":
+		return true, 6
+	case "seven":
+		return true, 7
+	case "eight":
+		return true, 8
+	case "nine":
+		return true, 9
+	}
+	return false, -1
 }
